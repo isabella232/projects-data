2bfadf79d7cf56c29b5398f0dc280105bf4c8517
xavoliva6 <xavi.oliva6@gmail.com>
2021-10-20 15:43:55 -0700

    add client visualization notebook

--- hyperparam_heuristic/heuristic_funcs.py
+++ hyperparam_heuristic/heuristic_funcs.py
@@ -1,20 +1,52 @@
+import numpy as np
+
 from utils import closest_power, filter_outliers
 
 
 def lr_heuristic(ratios, nr_parties, lrs, best_acc, val_accs, type_of_skew, v=0):
     agg_lr = 0
-    for i in range(nr_parties):
-        if type_of_skew == "feature":
-            # abs not in paper
-            agg_lr += lrs[i] * (1 - lrs[i]) / (10 * nr_parties) * (1 - abs(best_acc - val_accs[i]))
+    if type_of_skew == "feature":
+        if v == 0:
+            for i in range(nr_parties):
+                # abs not in paper
+                agg_lr += lrs[i] * (1 - lrs[i]) / (10 * nr_parties) * (1 - abs(best_acc - val_accs[i]))
 
-        if type_of_skew == "label":
-            # abs not in paper
-            agg_lr += lrs[i] * (1 - lrs[i]) * (1 - abs(best_acc - val_accs[i])) / (nr_parties * 10)
+        elif v == 1:
+            for i in range(nr_parties):
+                # remove best_acc and abs
+                agg_lr += lrs[i] * (1 - lrs[i]) / (10 * nr_parties)
 
-        if type_of_skew == "qty":
-            # abs not in paper
-            agg_lr += ratios[i] * lrs[i] * (1 - lrs[i]) * (1 - abs(best_acc - val_accs[i])) / 10
+        if v == 2:
+            # TODO
+            agg_lr = 0.032
+
+    if type_of_skew == "label":
+        if v == 0:
+            for i in range(nr_parties):
+                # abs not in paper
+                agg_lr += lrs[i] * (1 - lrs[i]) / (10 * nr_parties) * (1 - abs(best_acc - val_accs[i]))
+        elif v == 1:
+            for i in range(nr_parties):
+                # remove best_acc and abs
+                agg_lr += lrs[i] * (1 - lrs[i]) / (10 * nr_parties)
+
+        if v == 2:
+            # TODO
+            agg_lr = 0.032
+
+    if type_of_skew == "qty":
+        if v == 0:
+            for i in range(nr_parties):
+                # abs not in paper
+                agg_lr += ratios[i] * lrs[i] * (1 - lrs[i]) * (1 - abs(best_acc - val_accs[i])) / 10
+        elif v == 1:
+            for i in range(nr_parties):
+                # remove best_acc and abs
+                agg_lr += ratios[i] * lrs[i] * (1 - lrs[i]) / 10
+
+        if v == 2:
+            # TODO
+            agg_lr = .032
 
     return agg_lr
 
@@ -28,25 +60,41 @@ def momentum_heuristic(ratios, nr_parties, momentums, best_acc, val_accs, type_o
                 agg_mom += min(1 / nr_parties,
                                momentums[i] * (1 - momentums[i]) / nr_parties * (1 - abs(best_acc - val_accs[i])) * 10)
         elif v == 1:
+            # CAN'T FIND BETTER HEURISTIC THAN SIMPLE AVERAGE
+            agg_mom = np.mean(momentums)
+
+        elif v == 2:
+            # SIMPLE AVERAGE FILTERING OUTLIERS
             fil_moms = filter_outliers(momentums)
-            fil_nr_parties = len(fil_moms)
-            for mom in fil_moms:
-                agg_mom += mom / fil_nr_parties
+            agg_mom = np.mean(fil_moms)
 
     elif type_of_skew == "label":
-        for i in range(nr_parties):
-            agg_mom += momentums[i] / nr_parties
+        if v == 0:
+            agg_mom = np.mean(momentums)
+        elif v == 1:
+            # CAN'T FIND BETTER HEURISTIC THAN SIMPLE AVERAGE
+            agg_mom = np.mean(momentums)
+
+        elif v == 2:
+            # SIMPLE AVERAGE FILTERING OUTLIERS
+            fil_moms = filter_outliers(momentums)
+            agg_mom = np.mean(fil_moms)
 
     elif type_of_skew == "qty":
-        # abs not in paper
         if v == 0:
             for i in range(nr_parties):
+                # abs not in paper
                 agg_mom += min(1 / nr_parties,
                                ratios[i] * momentums[i] * (1 - momentums[i]) * (1 - abs(best_acc - val_accs[i])) * 10)
         elif v == 1:
-            for i in range(nr_parties):
-                # delete min, best_acc, val_acc
-                agg_mom += momentums[i] / nr_parties
+            # CAN'T FIND BETTER HEURISTIC THAN SIMPLE AVERAGE
+            agg_mom = np.mean(momentums)
+
+        elif v == 2:
+            # SIMPLE AVERAGE FILTERING OUTLIERS
+            fil_moms = filter_outliers(momentums)
+            agg_mom = np.mean(fil_moms)
+
     return agg_mom
 
 
@@ -61,13 +109,39 @@ def batch_size_heuristic(ratios, nr_parties, batch_sizes, val_accs, type_of_skew
             for i in range(nr_parties):
                 agg_bs += batch_sizes[i] / nr_parties
 
+        if v == 2:
+            # SIMPLE AVERAGE FILTERING OUTLIERS
+            fil_bs = filter_outliers(batch_sizes)
+            agg_bs += np.mean(fil_bs)
+
     elif type_of_skew == "label":
-        for i in range(nr_parties):
-            agg_bs += batch_sizes[i] * val_accs[i] / nr_parties
+        if v == 0:
+            for i in range(nr_parties):
+                agg_bs += batch_sizes[i] * val_accs[i] / nr_parties
+
+        elif v == 1:
+            for i in range(nr_parties):
+                agg_bs += batch_sizes[i] / nr_parties
+
+        if v == 2:
+            # SIMPLE AVERAGE FILTERING OUTLIERS
+            fil_bs = filter_outliers(batch_sizes)
+            agg_bs += np.mean(fil_bs)
+
 
     elif type_of_skew == "qty":
-        for i in range(nr_parties):
-            agg_bs += ratios[i] * batch_sizes[i] * val_accs[i]
+        if v == 0:
+            for i in range(nr_parties):
+                agg_bs += ratios[i] * batch_sizes[i] * val_accs[i]
+
+        elif v == 1:
+            for i in range(nr_parties):
+                agg_bs += batch_sizes[i] / nr_parties
+
+        if v == 2:
+            # SIMPLE AVERAGE FILTERING OUTLIERS
+            fil_bs = filter_outliers(batch_sizes)
+            agg_bs += np.mean(fil_bs)
 
     return closest_power(agg_bs)
 
--- hyperparam_heuristic/plot.py
+++ hyperparam_heuristic/plot.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -8,16 +10,15 @@ from heuristic_funcs import aggregate_results
 
 
 # TODO: separate aggregate_results from plot results function
-def plot_results(dataset, skews, nrs_parties, hyper_param, type_of_skew):
+def plot_results(dataset, skews, nrs_parties, hyper_param, type_of_skew, versions=(0, 1)):
     fig, ax = plt.subplots(nrows=len(skews), ncols=len(nrs_parties), figsize=(12, 20))
-    agg_aes = {"v0": [], "v1": []}
+    agg_errors = defaultdict(list)
     baseline_aes = {"mean": [], "median": [], "mode": []}
     for i, s in enumerate(skews):
         for j, p in enumerate(nrs_parties):
             hps, accs, best_acc, ratios = get_client_res(dataset, s, p, type_of_skew)
 
-            agg_params_v0 = aggregate_results(hps, accs, best_acc, ratios, type_of_skew, v=0)
-            agg_params_v1 = aggregate_results(hps, accs, best_acc, ratios, type_of_skew, v=1)
+            agg_params = [aggregate_results(hps, accs, best_acc, ratios, type_of_skew, v=v) for v in versions]
             mean_params = baseline_results(hps, np.mean)
             median_params = baseline_results(hps, np.median)
             if hyper_param == "batch_size":
@@ -26,15 +27,11 @@ def plot_results(dataset, skews, nrs_parties, hyper_param, type_of_skew):
 
             sns.boxplot(y=hps[hyper_param], showmeans=True, ax=ax[i, j])
             sns.swarmplot(y=hps[hyper_param], color=".25", ax=ax[i, j])
-            if hyper_param in ["lr", "momentum"]:
-                prefix = "server_"
-            else:
-                prefix = ""
+            prefix = "server_" if (hyper_param in ["lr", "momentum"]) else ""
 
             fedavg_res = fedavg_params[f"{prefix}{hyper_param}"]
 
-            heuristic_res_v0 = agg_params_v0[f"{prefix}{hyper_param}"]
-            heuristic_res_v1 = agg_params_v1[f"{prefix}{hyper_param}"]
+            heuristic_res = [agg_params[v][f"{prefix}{hyper_param}"] for v in versions]
 
             mean_res = mean_params[f"{prefix}{hyper_param}"]
             median_res = median_params[f"{prefix}{hyper_param}"]
@@ -42,15 +39,16 @@ def plot_results(dataset, skews, nrs_parties, hyper_param, type_of_skew):
             if hyper_param == "batch_size":
                 mode_res = mode_params[f"{prefix}{hyper_param}"]
 
-            ax[i, j].scatter(x=0, y=heuristic_res_v0, s=100, marker="o", c="red",
+            ax[i, j].scatter(x=0, y=heuristic_res[0], s=100, marker="o", c="red",
                              label="heuristic v0")
-            ax[i, j].scatter(x=0, y=heuristic_res_v1, s=100, marker="x", c="orange",
+            ax[i, j].scatter(x=0, y=heuristic_res[1], s=100, marker="x", c="orange",
                              label="heuristic v1")
             ax[i, j].scatter(x=0, y=fedavg_res, s=100, marker="o", c="green",
                              label="fedavg")
 
-            agg_aes["v0"].append(np.abs(heuristic_res_v0 - fedavg_res))
-            agg_aes["v1"].append(np.abs(heuristic_res_v1 - fedavg_res))
+            for v in versions:
+                agg_errors[v].append(heuristic_res[v] - fedavg_res)
+
             baseline_aes["mean"].append(np.abs(mean_res - fedavg_res))
             baseline_aes["median"].append(np.abs(median_res - fedavg_res))
             if hyper_param == "batch_size":
@@ -59,7 +57,7 @@ def plot_results(dataset, skews, nrs_parties, hyper_param, type_of_skew):
 
             ax[i, j].set_title(f"{len(ratios)} clients, {type_of_skew} skew {s}, {dataset}")
             ax[i, j].set_xlabel("Number of clients")
-            ax[i, j].set_ylabel(f"{hyper_param}")
+            ax[i, j].set_ylabel(hyper_param)
             ax[i, j].legend()
 
-    return agg_aes, baseline_aes
+    return agg_errors, baseline_aes
